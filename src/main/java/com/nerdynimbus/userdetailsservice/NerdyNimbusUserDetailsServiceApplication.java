4d6dbde625de00574f31a26fa0b1acbe156fe631
nerdynimbus <youtube@aux-tech.com>
2024-02-21 04:30:09 -0700

    first commit

---
+++ src/main/java/com/nerdynimbus/userdetailsservice/NerdyNimbusUserDetailsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nerdynimbus.userdetailsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdyNimbusUserDetailsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdyNimbusUserDetailsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nerdynimbus/userdetailsservice/NerdyNimbusUserDetailsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerdynimbus.userdetailsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerdyNimbusUserDetailsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
